e92525d8a07164a22a5490a01a19fd1fe7a006ec
Siddharth Bhat <siddharth.bath@tweag.io>
2019-07-20 18:05:30 +0200

    trying to get strength reduction to work

--- Main.hs
+++ Main.hs
@@ -23,16 +23,20 @@ import Control.Monad.Writer
 import Control.Monad.Except
 import Control.Monad.Identity
 import qualified Data.Map as M
+import Debug.Trace
 
 -- | unique labels
 type Uniq = Int
+--
+-- | Global state
+data G = G { guniq :: Uniq, gsymbols :: M.Map String (SBV Int32) }
 
 -- | Top level monad all our computations run in
-type M a =  StateT Uniq IO a
+type M a =  StateT G IO a
 
 -- | execute a computation inside our `M`
 evalM :: M a -> IO a
-evalM ma = evalStateT ma 0
+evalM ma = evalStateT ma (G 0 mempty)
 
 
 -- | Number of parameters for the program
@@ -42,6 +46,7 @@ type NParams = Int
 data SymE =
    SymInt Int32
    | SymSym String
+   | SymParam String
    | SymAdd SymE SymE
    | SymMul SymE SymE
    | SymLt SymE SymE
@@ -143,38 +148,61 @@ symbolicTransfer =
 runSymbolic :: Program SymE -> [SymE] -> Maybe SymE
 runSymbolic p as = interpretM symbolicTransfer p as
 
-compileSymbolicToSBV :: SymE -> (Symbolic (SBV Int32)
-compileSymbolicToSBV (SymInt x) = return $ fromIntegral x
-compileSymbolicToSBV (SymSym x) = symbolic x
-compileSymbolicToSBV (SymAdd s1 s2) =
-  liftA2 (+) (compileSymbolicToSBV s1)  (compileSymbolicToSBV s2)
-compileSymbolicToSBV (SymMul s1 s2) =
-  liftA2 (*) (compileSymbolicToSBV s1)  (compileSymbolicToSBV s2)
-compileSymbolicToSBV (SymLt s1 s2) = do
-  sbv1 <- compileSymbolicToSBV s1
-  sbv2 <- compileSymbolicToSBV s2
-  return $ ite  (sbv1 .< sbv2) 1 0
-compileSymbolicToSBV (SymIte i t e) = do
-  sbvi <- compileSymbolicToSBV i
-  sbvt <- compileSymbolicToSBV t
-  sbve <- compileSymbolicToSBV e
-  return $ ite  (sbvi .== 1) sbvt sbve
+compileSymbolicToSBV :: M.Map String (SInt32) ->
+  SymE ->
+  Symbolic (SInt32, M.Map String (SInt32))
+compileSymbolicToSBV m (SymInt x) = return $ (fromIntegral x, m)
+compileSymbolicToSBV m (SymSym name) =
+  case m M.!? name of
+    Just sym -> return ( sym, m)
+    Nothing -> do
+       sym <- sInt32 name
+       return $ (sym, M.insert name sym m)
+compileSymbolicToSBV m (SymParam name) = do
+  case m M.!? name of
+    Just sym -> return ( sym, m)
+    Nothing -> do
+       sym <- forall name
+       return $ (sym, M.insert name sym m)
+compileSymbolicToSBV m (SymAdd s1 s2) = do
+  (s1, m) <- (compileSymbolicToSBV m s1)
+  (s2, m) <- (compileSymbolicToSBV m s2)
+  return $ (s1 + s2, m)
+
+compileSymbolicToSBV m (SymMul s1 s2) = do
+  (s1, m) <- (compileSymbolicToSBV m s1)
+  (s2, m) <- (compileSymbolicToSBV m s2)
+  return $ ( s1 * s2, m)
+
+compileSymbolicToSBV m (SymLt s1 s2) = do
+  (s1, m) <- (compileSymbolicToSBV m s1)
+  (s2, m) <- (compileSymbolicToSBV m s2)
+  return $ (ite (s1 .< s2) 1 0, m)
+
+compileSymbolicToSBV m (SymIte i t e) = do
+  (i, m) <- (compileSymbolicToSBV m i)
+  (t, m) <- (compileSymbolicToSBV m t)
+  (e, m) <- (compileSymbolicToSBV m e)
+  return $ (ite  (i .== 1) t e, m)
 
 
 -- | generate a unique label
 genuniq :: M Uniq
-genuniq = state $ \u -> (u, u+1)
+genuniq = state $ \G{..} -> (guniq, G{ guniq=guniq+1, ..})
 
 -- | provide a random integer in [lo, hi]
 randint :: (Int, Int) -> M Int
 randint (lo, hi) = liftIO $ getStdRandom $ randomR (lo, hi)
+
+randint32 :: (Int32, Int32) -> M Int32
+randint32 (lo, hi) = liftIO $ getStdRandom $ randomR (lo, hi)
 
 -- | random uniform float
 randfloat :: (Float, Float) -> M Float
 randfloat (lo, hi) = liftIO $ getStdRandom $ randomR (lo, hi)
 
 -- | Generate a random instruction.
-randSymInst ::  StateT Uniq IO (Inst SymE)
+randSymInst ::  M (Inst SymE)
 randSymInst = do
     choice <- randint (1, 4)
     case choice of
@@ -189,11 +217,17 @@ randSymInst = do
 -- | Reify a symbolic instruction into a real instruction
 -- by consulting the dictionary.
 reifySymInst :: SatResult -> Inst SymE -> Inst Int32
+reifySymInst res (Push (SymParam s)) = error $ "param: " <> s
 reifySymInst res (Push (SymSym s)) =
-  let Just (v :: Int32) = getModelValue s res in Push v
+  let Just (v :: Int32) = trace ("s: " <> s) $ getModelValue s res in Push v
 reifySymInst _ Add = Add
 reifySymInst _ Mul = Mul
 reifySymInst _ Lt = Lt
+
+-- | Given a symbolic program and an SBV model dictionary,
+-- reify it into an actual program
+reifySymProgram :: SatResult -> Program SymE -> Program Int32
+reifySymProgram res s = trace ("reifyp: " <> show s) $ map (reifySymInst  res) s
 
 -- | Convert a concrete program into a symbolic program
 concrete2symInst :: Inst Int32 -> Inst SymE
@@ -206,10 +240,6 @@ concrete2symInst Lt = Lt
 concrete2symProgram :: Program Int32 -> Program SymE
 concrete2symProgram = map concrete2symInst
 
--- | Given a symbolic program and an SBV model dictionary,
--- reify it into an actual program
-reifySymProgram :: SatResult -> Program SymE -> Program Int32
-reifySymProgram res = map (reifySymInst  res)
 
 -- | Generate a random program
 randSymProgram :: Int -- ^ max length
@@ -243,7 +273,7 @@ unifySymProgram ::
   -> M (Maybe (Program Int32))
 unifySymProgram nparams c s = do
   ids <- replicateM nparams genuniq
-  let params = [SymSym ("param-" <> show i) | i <- ids]
+  let params = [SymParam ("param-" <> show i) | i <- ids]
   -- | Run the program as a symbolic effect on the stack
   let mvc = runSymbolic (concrete2symProgram c) params
   -- | Run the symbolic program as the same
@@ -254,8 +284,8 @@ unifySymProgram nparams c s = do
       Just (vc, vs) -> do
         -- | We need to be careful here to not double allocate te
         -- same symbol
-        sbvc <- compileSymbolicToSBV vc
-        sbvs <- compileSymbolicToSBV vs
+        (sbvc, m) <- compileSymbolicToSBV mempty vc
+        (sbvs, m) <- compileSymbolicToSBV m vs
         return $ sbvc .== sbvs
 
   if not (modelExists smtResult)
@@ -270,11 +300,11 @@ scoreSymProgram :: NParams
 scoreSymProgram nparams c s = do
   msol <- unifySymProgram nparams c s
   case msol of
-    Nothing -> return 0.0 -- ^ let there be some chance of accepting
-                          --   invalid programs
-    Just sol ->
-      let Just (_, cost) = runCost concreteTransfer sol []
-       in return $ 2.0 ** (-1.0 * fromIntegral (getCost cost))
+    Nothing -> return 0.0
+    Just sol -> do
+      inputs <- replicateM nparams (randint32 (1, 4))
+      let Just (_, cost) = runCost concreteTransfer sol inputs
+      return $ 2.0 ** (-1.0 * fromIntegral (getCost cost))
 
 
 
@@ -285,7 +315,7 @@ mhStep :: NParams ->
 mhStep nparams c s = do
   a <- scoreSymProgram nparams c s
   -- | perturb the current program to get the next program
-  s' <- randSymProgram (length c * 2)-- perturbSymProgram s
+  s' <- randSymProgram (length c * 2) -- perturbSymProgram s
   a' <- scoreSymProgram nparams c s'
   -- | find acceptance ratio
   let accept = a' / a
@@ -326,11 +356,13 @@ mainProgram :: NParams -> Program Int32 -> M ()
 mainProgram nparams p = do
     liftIO $ putStrLn $ "----"
     liftIO $ putStrLn $ "program: " <> show p
-    liftIO $ print $ runConcrete p []
+    let params = [SymParam $ "p-" <> show i | i <- [1..nparams]]
+    liftIO $ print $ runSymbolic (concrete2symProgram p) params
     opts <- optimise nparams p
-    forM_ opts $ \p -> do
-          liftIO $ print p
-          let Just (_, cost) =  runCost concreteTransfer p []
+    forM_ opts $ \p' -> do
+          liftIO $ print p'
+          liftIO $ print $ runSymbolic (concrete2symProgram p') params
+          let Just (_, cost) =  runCost symbolicTransfer (concrete2symProgram p') params
           liftIO $ putStrLn $ "  cost: " <> show cost
 
 main :: IO ()
